db97683122d17bcbde5ffa84ca17a738f50c7f54
Tanya Labetskaya <tanya.labetskaya@y42.com>
2024-05-08 23:18:47 +0200

    added trip info view

--- src/presenter/general-presenter.js
+++ src/presenter/general-presenter.js
@@ -1,11 +1,17 @@
 import {render, RenderPosition} from '../render.js';
 import SortingView from '../view/sorting.js';
 import FiltersView from '../view/filters.js';
+import TripInfoView from '../view/trip-info.js';
 
 export default class GeneralPresenter {
   constructor() {
+    this.tripInfoElement = document.querySelector('.trip-main');
     this.tripEventsSectionElement = document.querySelector('.trip-events');
     this.filtersSectionElement = document.querySelector('.trip-controls__filters');
+  }
+
+  renderTripInfo() {
+    render(new TripInfoView(), this.tripInfoElement, RenderPosition.AFTERBEGIN);
   }
 
   renderSorting() {
@@ -17,6 +23,7 @@ export default class GeneralPresenter {
   }
 
   init() {
+    this.renderTripInfo();
     this.renderSorting();
     this.renderFilters();
   }
