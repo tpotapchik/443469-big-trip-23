1ea72633172689a89f938acdb549d4fb22dbc058
Tanya Labetskaya <tanya.labetskaya@y42.com>
2024-05-08 23:08:12 +0200

    added filters view

--- src/constans.js
+++ src/constans.js
@@ -1,3 +1,4 @@
 const SORTING_TYPES = ['Day', 'Event', 'Time', 'Price', 'Offers'];
+const FILTER_TYPES = ['Everything', 'Future', 'Present', 'Past'];
 
-export {SORTING_TYPES};
+export {SORTING_TYPES, FILTER_TYPES};
--- src/presenter/general-presenter.js
+++ src/presenter/general-presenter.js
@@ -1,18 +1,24 @@
 import {render, RenderPosition} from '../render.js';
 import SortingView from '../view/sorting.js';
+import FiltersView from '../view/filters.js';
 
 export default class GeneralPresenter {
   constructor() {
     this.tripEventsSectionElement = document.querySelector('.trip-events');
+    this.filtersSectionElement = document.querySelector('.trip-controls__filters');
   }
 
   renderSorting() {
     render(new SortingView(), this.tripEventsSectionElement);
   }
 
+  renderFilters() {
+    render(new FiltersView(), this.filtersSectionElement);
+  }
 
   init() {
     this.renderSorting();
+    this.renderFilters();
   }
 }
 
