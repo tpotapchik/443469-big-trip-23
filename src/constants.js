73e2e6402050c5269168c49e9f6d5cd68694bae3
Tanya <tanyapotapchik@gmai.com>
2024-06-05 22:21:37 +0200

    1. fix sorting

--- src/constants.js
+++ src/constants.js
@@ -14,7 +14,7 @@ const FilterType = {
 };
 
 const FilterMessage = {
-  [FilterType.EVERYTHING]: 'Click "New Event" to create your first point',
+  [FilterType.EVERYTHING]: 'Click New Event to create your first point',
   [FilterType.FUTURE]: 'There are no future events now',
   [FilterType.PRESENT]: 'There are no present events now',
   [FilterType.PAST]: 'There are no past events now'
--- src/utils/sorting-values.js
+++ src/utils/sorting-values.js
@@ -1,10 +1,12 @@
 import {SortingType} from '../constants.js';
 
-const getTimeDifference = ({dateFrom, dateTo}) => (new Date(dateTo).getTime() - new Date(dateFrom).getTime());
-
 const sortBy = {
   [SortingType.DAY]: (points) => [...points].sort((a, b) => new Date(a.dateFrom) - new Date(b.dateFrom)),
-  [SortingType.TIME]: (points) => [...points].sort((a, b) => getTimeDifference(b) - getTimeDifference(a)),
+  [SortingType.TIME]: (points) => [...points].sort((a, b) => {
+    const durationA = new Date(a.dateTo).getTime() - new Date(a.dateFrom).getTime();
+    const durationB = new Date(b.dateTo).getTime() - new Date(b.dateFrom).getTime();
+    return durationB - durationA;
+  }),
   [SortingType.PRICE]: (points) => [...points].sort((a, b) => b.basePrice - a.basePrice),
 };
 
